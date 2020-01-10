2c4e3766bd9338240859fe78029f25f9c94c7906
YangJiaming <yangjiaming@9fbank.cc>
2020-01-10 19:05:36 +0800

    docs(detection):增加方法注释

--- src/detecting.js
+++ src/detecting.js
@@ -1,28 +1,63 @@
+/**
+ * 获取UA
+ *
+ * @param {any} lower 是否转小写
+ * @returns {string} UA
+ */
 const ua = (lower) => {
   return lower ? window.navigator.userAgent.toLowerCase() : window.navigator.userAgent;
 };
 
+/**
+ * 是否为iOS系统
+ *
+ * @returns {boolean} 是否为iOS系统
+ */
 const isIOS = () => {
   return /iPhone|iPad|iPod/.test(ua());
 };
 
+/**
+ * 是否为iPhone设备
+ *
+ * @returns {boolean} 是否为iPhone设备
+ */
 const isIPhone = () => {
   return /iPhone/.test(ua());
 };
 
+/**
+ * 是否为iPad设备
+ *
+ * @returns {boolean} 是否为iPad设备
+ */
 const isIPad = () => {
   return /iPad/.test(ua());
 };
 
+/**
+ * 是否为Android系统
+ *
+ * @returns {boolean} 是否为Android系统
+ */
 const isAndroid = () => {
   return /Android/i.test(ua());
 };
 
+/**
+ * 是否为微信客户端
+ *
+ * @returns {boolean} 是否为微信客户端
+ */
 const isWeiXin = () => {
   return /MicroMessenger/i.test(ua());
 };
 
-// X XS, XS Max, XR, 11, 11 Pro, 11 Pro Max
+/**
+ * 是否为iPhone刘海屏机型：X XS, XS Max, XR, 11, 11 Pro, 11 Pro Max
+ *
+ * @returns {boolean} 是否为微信客户端
+ */
 const isNotchIPhone = () => {
   const screens = [
     { dpr: 3, width: 375, height: 812 },
