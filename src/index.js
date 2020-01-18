69921b84542bcc2f180671b424c1d069d9a2019d
JamieYang <jm.y.null@gmail.com>
2020-01-19 00:24:47 +0800

    feat:新增url、cookie模块

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import * as array from './array';
+import * as bom from './bom';
+import * as cookie from './cookie';
+import * as detecting from './detecting';
+import * as dom from './dom';
+import * as format from './format';
+import * as func from './function';
+import * as image from './image';
+import * as lang from './lang';
+import * as object from './object';
+import * as random from './random';
+import * as string from './string';
+import * as url from './url';
+
+export default {
+  ...array,
+  ...bom,
+  ...cookie,
+  ...detecting,
+  ...dom,
+  ...format,
+  ...func,
+  ...image,
+  ...lang,
+  ...object,
+  ...random,
+  ...string,
+  ...url,
+};
--- src/url.js
+++ src/url.js
@@ -1,20 +1,34 @@
 /**
+ *
+ * 获取URL的查询参数对象
+ *
+ * @param {string} [url] 链接
+ * @param {string} [section] 位置 search\hash
+ * @returns {object} 参数对象
+ */
+const getURLParams = (url = window.location.href, section = 'search') =>
+  url.match(/([^?=&]+)(=([^&]*))/g).reduce((a, v) => {
+    a[v.slice(0, v.indexOf('='))] = v.slice(v.indexOf('=') + 1);
+    return a;
+  }, {});
+
+/**
  * 获取URL上指定的查询参数
  *
- * @param {string} name 参数名称
+ * @param {string} key 参数名
  * @returns {string} 指定参数的值
  */
-export const getUrlParam = (name) => {
-  let re = new RegExp('(^|&)' + name + '=([^&]*)(&|$)');
+const getUrlParam = (key) => {
+  const re = new RegExp('([?|&])' + key + '=([^&]*)(&|$)');
+  const arrHash = window.location.hash.split('?');
+  const arrSearch = window.location.search.substr(1).split('?');
   let value = '';
-  let arrHash = window.location.hash.split('?');
-  let arrSearch = window.location.search.substr(1).split('?');
 
   arrHash.shift();
-  let arr = arrSearch[0] !== '' ? arrSearch : arrHash;
+  const arr = arrSearch[0] !== '' ? arrSearch : arrHash;
 
   for (let i = 0; i < arr.length; i++) {
-    let r = arr[i].match(re);
+    const r = arr[i].match(re);
     if (r !== null && r[2]) {
       value = r[2];
       break;
@@ -24,13 +38,105 @@ export const getUrlParam = (name) => {
 };
 
 /**
- * 获取URL的查询参数对象
+ * setUrlParam
  *
- * @param {string} [url] 链接
- * @returns {object} 参数对象
+ * @see https://stackoverflow.com/questions/5999118/add-or-update-query-string-parameter
+ * @param {string} key 参数名
+ * @param {string} value 参数值
+ * @param {string} [url] URL链接
+ * @returns {string} 处理后的完整URL
  */
-export const getURLParams = (url = window.location.href) =>
-  url.match(/([^?=&]+)(=([^&]*))/g).reduce((a, v) => {
-    a[v.slice(0, v.indexOf('='))] = v.slice(v.indexOf('=') + 1);
-    return a;
-  }, {});
+const setUrlParam = (key, value, url = window.location.href) => {
+  const re = new RegExp('([?|&])' + key + '=.*?(&|#|$)', 'i');
+
+  if (url.match(re)) {
+    return url.replace(re, '$1' + key + '=' + encodeURIComponent(value) + '$2');
+  } else {
+    let hash = '';
+    if (url.indexOf('#') !== -1) {
+      hash = url.replace(/.*#/, '#');
+      url.replace(/#.*/, '');
+    }
+    let separator = url.indexOf('?') !== -1 ? '&' : '?';
+    return url + separator + key + '=' + encodeURIComponent(value) + hash;
+  }
+};
+
+const deleteUrlParam = (param, url = window.location.href) => {
+  // prefer to use l.search if you have a location/link object
+  let urlparts = url.split('?');
+  if (urlparts.length >= 2) {
+    let prefix = encodeURIComponent(param) + '=';
+    let pars = urlparts[1].split(/[&;]/g);
+
+    // reverse iteration as may be destructive
+    for (let i = pars.length; i-- > 0; ) {
+      // idiom for string.startsWith
+      if (pars[i].lastIndexOf(prefix, 0) !== -1) {
+        pars.splice(i, 1);
+      }
+    }
+    return urlparts[0] + (pars.length > 0 ? '?' + pars.join('&') : '');
+  } else {
+    return url;
+  }
+};
+
+/**
+ * 对象序列化
+ * 支持一层对象
+ *
+ * @param {JSON} obj JSON对象
+ * @returns {string} 序列化查询参数串
+ */
+const stringifyQueryString = (obj) => {
+  if (!obj) return '';
+  let pairs = [];
+
+  for (let key in obj) {
+    if ({}.hasOwnProperty.call(obj, key)) {
+      let value = obj[key];
+
+      if (value instanceof Array) {
+        pairs = pairs.concat(value.map((v, i) => `${key}[${i}]=${v}`));
+        continue;
+      }
+
+      pairs.push(`${key}=${obj[key]}`);
+    }
+  }
+
+  return pairs.join('&');
+};
+
+/**
+ * url参数转对象
+ *
+ * @param  {string} [url] default: window.location.href
+ * @returns {object} 结果对象
+ */
+function parseQueryString(url = window.location.href) {
+  if (url.indexOf('?') === -1) {
+    return {};
+  }
+  let search = url[0] === '?' ? url.substr(1) : url.substring(url.lastIndexOf('?') + 1);
+  if (search === '') {
+    return {};
+  }
+  search = search.split('&');
+  let query = {};
+  for (let i = 0; i < search.length; i++) {
+    let pair = search[i].split('=');
+    query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+  }
+  return query;
+}
+
+export {
+  getURLParams,
+  getUrlParam,
+  setUrlParam,
+  deleteUrlParam,
+  stringifyQueryString,
+  parseQueryString,
+};
