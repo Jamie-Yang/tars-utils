a3589f8f4c88b7d7af926457112983a4e47df2dd
JamieYang <jm.y.null@gmail.com>
2020-05-31 15:27:29 +0800

    feat(array): 增加 shuffle 乱序方法

---
+++ src/array/shuffle.js
@@ -0,0 +1,18 @@
+/**
+ * 乱序（洗牌算法）
+ *
+ * @param {Array} arr 数组
+ * @returns {Array} 经过随机乱序后的数组
+ */
+const shuffle = (arr = []) => {
+  let cursor = arr.length;
+
+  while (--cursor) {
+    const random = Math.floor(Math.random() * cursor);
+    [arr[cursor], arr[random]] = [arr[random], arr[cursor]];
+  }
+
+  return arr;
+};
+
+export default shuffle;
