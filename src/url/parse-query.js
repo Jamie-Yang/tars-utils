086cad8ffbcdb0d3183dd078559d5c5743279cfe
JamieYang <jm.y.null@gmail.com>
2021-07-26 23:20:30 +0800

    feat(url): 完善方法注释

--- src/url/parse-query.js
+++ src/url/parse-query.js
@@ -1,10 +1,10 @@
 import removeHash from './remove-hash';
 
 /**
- * 解析 URL 参数字符串为对象
+ * 解析 URL 查询参数字符串为对象
  *
- * @param {string} [url] 参数字段
- * @returns {object} 参数对象
+ * @param {string} [url] 查询参数字符串，支持完整 URL
+ * @returns {object} 查询参数对象
  */
 export default function parseQuery(url = window.location.href) {
   const _url = removeHash(url);
--- src/url/stringify-query.js
+++ src/url/stringify-query.js
@@ -1,9 +1,9 @@
 /**
- * 对象序列化
+ * 查询参数对象序列化
  * 支持一层对象
  *
- * @param {JSON} obj JSON对象
- * @returns {string} 序列化查询参数串
+ * @param {JSON} obj 查询参数对象，JSON 子集，不支持数组类型
+ * @returns {string} 查询参数序列化后的字符串
  */
 export default function stringifyQuery(obj) {
   if (!obj) return '';
