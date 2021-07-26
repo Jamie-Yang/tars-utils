946414c72885030183477b23cc6edcb460512854
JamieYang <jm.y.null@gmail.com>
2021-07-27 00:38:51 +0800

    feat(url): 使parseQuery 专门用于解析查询字符串

--- src/url/parse-query.js
+++ src/url/parse-query.js
@@ -1,14 +1,11 @@
-import removeHash from './remove-hash';
-
 /**
  * 解析 URL 查询参数字符串为对象
  *
- * @param {string} [url] 查询参数字符串，支持完整 URL
+ * @param {string} qs 查询参数字符串
  * @returns {object} 查询参数对象
  */
-export default function parseQuery(url = window.location.href) {
-  const _url = removeHash(url);
-  return (_url.match(/([^?=&]+)(=([^&]*))/g) || []).reduce((a, v) => {
+export default function parseQuery(qs) {
+  return (qs.match(/([^?=&]+)(=([^&]*))/g) || []).reduce((a, v) => {
     const pair = v.split('=');
     a[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
     return a;
