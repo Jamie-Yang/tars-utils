83aa050acc70f0aacfb032b2133cb04942acefd0
JamieYang <jm.y.null@gmail.com>
2021-07-29 00:27:50 +0800

    feat(url): 增加修改增加 URL 参数的方法，setSearchParams

---
+++ src/url/set-search-params.js
@@ -0,0 +1,17 @@
+import parseUrl from './parse-url';
+import stringifyQuery from './stringify-query';
+
+/**
+ * 修改 URL 参数，没有则新增
+ *
+ * @param {object} query 参数对象
+ * @param {string} url URL
+ * @returns {string} 修改参数后的 URL
+ */
+export default function setSearchParams(query, url = window.location.href) {
+  const parsedUrl = parseUrl(url);
+  const queryString = stringifyQuery({ ...parsedUrl.query, ...query });
+  const search = queryString ? `?${queryString}` : '';
+
+  return `${parsedUrl.url}${search}${parsedUrl.hash}`;
+}
